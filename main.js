d44f1456529cf4f345547ba6488243eb2944df34
Alexandre Briffaut <briffaut.alexandre62@gmail.com>
2019-01-14 15:19:54 +0100

    First commit

---
+++ main.js
@@ -0,0 +1,78 @@
+var timer = document.getElementById("timer")
+var startBtn = document.getElementById("startBtn")
+var stopBtn = document.getElementById("stopBtn")
+var resetBtn = document.getElementById("resetBtn")
+var saveBtn = document.getElementById("saveBtn")
+var deleteBtn = document.getElementById("deleteBtn")
+var table = []
+
+table = localStorage.getItem("tableData")
+
+var startTime = 0
+var start = 0
+var end = 0
+var diff = 0
+var timerID = 0
+function chrono() {
+    end = new Date()
+    diff = end - start
+    diff = new Date(diff)
+    var msec = diff.getMilliseconds()
+    var sec = diff.getSeconds()
+    var min = diff.getMinutes()
+    var hr = diff.getHours() - 1
+    if (min < 10) {
+        min = "0" + min
+    }
+    if (sec < 10) {
+        sec = "0" + sec
+    }
+    if (msec < 10) {
+        msec = "00" + msec
+    }
+    else if (msec < 100) {
+        msec = "0" + msec
+    }
+    timer.innerHTML = hr + ":" + min + ":" + sec + ":" + msec
+    timerID = setTimeout("chrono()", 10)
+}
+// On écoute le bouton "startBtn" et exécute la fonction chronoStart.
+startBtn.addEventListener("click", chronoStart)
+// Fonction qui lance le chronomètre et le met en pause
+function chronoStart() {
+    if(startBtn.innerHTML == "Start" && timer == "00:00:00:00") {
+    start = new Date()
+    chrono()
+    startBtn.innerHTML = "Stop"
+    }
+    else if (startBtn.innerHTML == "Start" && timer != "00:00:00:00"){
+    start = new Date()-diff
+	start = new Date(start)
+    chrono()
+    startBtn.innerHTML = "Stop"
+    }
+    else {
+    clearTimeout(timerID)
+    startBtn.innerHTML = "Start"
+    }
+}
+// On écoute le bouton "resetBtn" et exécute la fonction chronoReset
+resetBtn.addEventListener("click", chronoReset)
+// Fonction qui remet à 0 le chronomètre
+function chronoReset(){
+	timer.innerHTML = "0:00:00:000"
+    start = new Date()   
+}
+// On écoute le bouton "deleteBtn" et exécute la fonction removeTimer
+deleteBtn.addEventListener("click", removeTimer)
+// Cette fonction vide le local storage
+function removeTimer(event) { 
+  localStorage.clear()
+}
+
+// On écoute le bouton "saveBtn" et exécute la fonction saveTimer
+saveBtn.addEventListener("click", saveTimer);
+// Cette fonction sauvegarde dans le localStorage le timer ainsi que la value inscrite dans l'input
+function saveTimer(event) {
+    localStorage.setItem(nameInput.value, JSON.stringify(timer.innerHTML), "tableData")
+}
